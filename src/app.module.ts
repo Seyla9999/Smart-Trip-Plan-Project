ab6b333c3272e36c7e8144e1e04644b9297a5fbf
root <root@DESKTOP-KPD5D9S.localdomain>
2026-04-30 08:06:23 +0700

    Fix debug

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { BookmarksModule } from './modules/bookmarks/bookmarks.module'
 import { AttractionsModule } from './modules/attractions/attractions.module'
+import { TripsModule } from './modules/trips/trips.module'
 import { UserPreferences } from './modules/users/user-preferences.entity'
 import { Bookmark } from './modules/bookmarks/bookmark.entity'
 import { Attraction } from './modules/attractions/attraction.entity'
@@ -56,6 +57,7 @@ import { UsersModule } from './modules/users/users.module'
     UsersModule,
     BookmarksModule,
     AttractionsModule,
+    TripsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
