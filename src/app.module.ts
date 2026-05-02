50181c24952a991a5dc7f064e35d6da506c4903e
Sarith-Seyla <167552287+Sarith-Seyla@users.noreply.github.com>
2026-05-02 18:16:08 +0700

    Remove AttractionsModule import from app.module.tsfix

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,6 @@ import { AuthModule } from './modules/auth/auth.module';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { HttpModule } from '@nestjs/axios';
 import { ProvincesModule } from './modules/home/provinces/provinces.module';
-import { AttractionsModule } from './modules/home/attractions/attractions.module';
 import { StoriesModule } from './modules/home/stories/story.module';
 import { SponsorsModule } from './modules/home/sponsors/sponsors.modules';
 import { WeatherModule } from './modules/home/weather/weather.module';
